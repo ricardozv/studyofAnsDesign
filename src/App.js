bfc3e4af2f23251a67dc182405d8e64fa33e5326
ricardozv <ricardosilvanet03@gmail.com>
2020-09-05 04:15:04 -0300

    uploads

--- src/App.js
+++ src/App.js
@@ -1,23 +1,65 @@
-import React, { useState } from 'react';
-import { render } from 'react-dom';
-import { DatePicker, message, Alert } from 'antd';
-import 'antd/dist/antd.css';
-import './index.css';
+const {  Layout, Menu, Breadcrumb  } = antd;
+const { 
+  DesktopOutlined,
+  PieChartOutlined,
+  FileOutlined,
+  TeamOutlined,
+  UserOutlined,
+ } = icons;
 
-const App = () => {
-  const [date, setDate] = useState(null);
-  const handleChange = value => {
-    message.info(`Selected Date: ${value ? value.format('YYYY-MM-DD') : 'None'}`);
-    setDate(value);
+const { Header, Content, Footer, Sider } = Layout;
+const { SubMenu } = Menu;
+
+class SiderDemo extends React.Component {
+  state = {
+    collapsed: false,
   };
-  return (
-    <div style={{ width: 400, margin: '100px auto' }}>
-      <DatePicker onChange={handleChange} />
-      <div style={{ marginTop: 16 }}>
-        Selected Date: {date ? date.format('YYYY-MM-DD') : 'None'}
-      </div>
-    </div>
-  );
-};
 
-render(<App />, document.getElementById('root'));
+  onCollapse = collapsed => {
+    console.log(collapsed);
+    this.setState({ collapsed });
+  };
+
+  render() {
+    return (
+      <Layout style={{ minHeight: '100vh' }}>
+        <Sider collapsible collapsed={this.state.collapsed} onCollapse={this.onCollapse}>
+          <div className="logo" />
+          <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
+            <Menu.Item key="1" icon={<PieChartOutlined />}>
+              Option 1
+            </Menu.Item>
+            <Menu.Item key="2" icon={<DesktopOutlined />}>
+              Option 2
+            </Menu.Item>
+            <SubMenu key="sub1" icon={<UserOutlined />} title="User">
+              <Menu.Item key="3">Tom</Menu.Item>
+              <Menu.Item key="4">Bill</Menu.Item>
+              <Menu.Item key="5">Alex</Menu.Item>
+            </SubMenu>
+            <SubMenu key="sub2" icon={<TeamOutlined />} title="Team">
+              <Menu.Item key="6">Team 1</Menu.Item>
+              <Menu.Item key="8">Team 2</Menu.Item>
+            </SubMenu>
+            <Menu.Item key="9" icon={<FileOutlined />} />
+          </Menu>
+        </Sider>
+        <Layout className="site-layout">
+          <Header className="site-layout-background" style={{ padding: 0 }} />
+          <Content style={{ margin: '0 16px' }}>
+            <Breadcrumb style={{ margin: '16px 0' }}>
+              <Breadcrumb.Item>User</Breadcrumb.Item>
+              <Breadcrumb.Item>Bill</Breadcrumb.Item>
+            </Breadcrumb>
+            <div className="site-layout-background" style={{ padding: 24, minHeight: 360 }}>
+              Bill is a cat.
+            </div>
+          </Content>
+          <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
+        </Layout>
+      </Layout>
+    );
+  }
+}
+
+ReactDOM.render(<SiderDemo />, mountNode);
--- src/index.js
+++ src/index.js
@@ -3,21 +3,16 @@ import { render } from 'react-dom';
 import { DatePicker, message, Alert } from 'antd';
 import 'antd/dist/antd.css';
 import './index.css';
+import {Button} from 'antd';
 
-const App = () => {
-  const [date, setDate] = useState(null);
-  const handleChange = value => {
-    message.info(`Selected Date: ${value ? value.format('YYYY-MM-DD') : 'None'}`);
-    setDate(value);
-  };
+function App() {
   return (
-    <div style={{  marginTop: 20}}>
-        <DatePicker onChange={value => this.handleChange(value)} />
-      <div style={{ marginTop: 20 }}>
-      <Alert message="Selected Date" description={date ? date.format('YYYY-MM-DD') : 'None'} />
-      </div>
+    <div className="App">
+      <header className="App-header">
+        <Button>Fala!</Button>
+      </header>
     </div>
-  );
-};
+  )
+}
 
 render(<App />, document.getElementById('root'));
