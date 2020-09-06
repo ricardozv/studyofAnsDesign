da8b993449c7007a485fda68cf81bfbd0c7c5800
ricardozv <ricardosilvanet03@gmail.com>
2020-09-06 01:06:54 -0300

    inicio do header

--- src/App.js
+++ src/App.js
@@ -2,34 +2,25 @@ import React from 'react';
 import {Button} from 'antd';
 import './App.css';
 
+import { Layout, Menu, Breadcrumb } from 'antd';
+import { UserOutlined, LaptopOutlined, NotificationOutlined } from '@ant-design/icons';
+
+const { SubMenu } = Menu;
+const { Header, Content, Sider } = Layout;
+
 
 function App() {
   return (
-    <div className="App">
-    <Button type="primary">Primary</Button>
-    <Button>Default</Button>
-    <Button type="dashed">Dashed</Button>
-    <Button type="danger">Danger</Button>
-    <Button type="link">Link</Button>
-
-    <Button>Default</Button>
-    <Button disabled>Default(disabled)</Button>
-
-    <Button type="primary" block>
-      Primary
-    </Button>
-    <Button block>Default</Button>
-    <Button type="dashed" block>
-      Dashed
-    </Button>
-    <Button type="danger" block>
-      Danger
-    </Button>
-    <Button type="link" block>
-      Link
-    </Button>
-    
+    <div className="App"> 
+    <Header className="header">
+      <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']}>
+        <Menu.Item key="1">Taxonomies</Menu.Item>
+        <Menu.Item key="2">API</Menu.Item>
+        <Menu.Item key="3">Documentation</Menu.Item>
+      </Menu>
+    </Header>
     </div>
+    
   );
 }
 
