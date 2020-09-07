17b08621b5312c5b1c7b56387f314c97b3d99063
ricardozv <ricardosilvanet03@gmail.com>
2020-09-07 17:02:23 -0300

    renomeia todos os components

---
+++ src/components/rank/Rank.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './index.css';
+
+
+export default function Rank() {
+    return(
+        <div className="login-container">
+            <form>
+            <img src={logo} alt = "Tindev" />
+            <input 
+            placeholder = "Digite seu nick do github"/>
+
+            <button type="submit"> enviar</button>
+            </form>
+        </div>  
+    );
+}
