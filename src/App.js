36aa1615f91c4bd0f1fae90cf83833bcf51962f5
ricardozv <ricardosilvanet03@gmail.com>
2020-09-08 12:41:41 -0300

    inicia a estruturaçao dos butoes na pagina

--- src/App.js
+++ src/App.js
@@ -1,47 +1,146 @@
-import React from 'react';
-import api from './services/api';
+// import api from './services/api';
 
-import { Row, Col } from 'antd';
-import { Layout, Menu, Breadcrumb } from 'antd';
+import { Divider } from 'antd';
+import React from 'react';
+import { Button } from 'antd';
+import { SearchOutlined } from '@ant-design/icons';
+import { Layout, Menu} from 'antd';
 import './global.css';
 
-
-const { SubMenu } = Menu;
-
-
-const { Header, Footer, Sider, Content } = Layout;
-
+const { Header, Footer, Content } = Layout;
 
 function App() {
   return (
     <div className="App"> 
     <Header className="header">
-      <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']}>
-        <Menu.Item key="1">Taxonomies</Menu.Item>
-        <Menu.Item key="2">API</Menu.Item>
-        <Menu.Item key="3">Documentation</Menu.Item>
+      <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['1']}>
+      <Divider type="vertical" />
+        <Button type= "primary">Taxonomies</Button>
+        <Divider type="vertical" />
+        <Button type="primary">Api</Button>
+        <Divider type="vertical" />
+        <Button type="primary">Documentasion</Button>
+        
+        <Divider type="vertical" />
+        <Button type="primary">Primary Button</Button>
+        <Divider type="vertical" />
+        <Button>Default Button</Button>
+        <Divider type="vertical" />
+        <Button type="dashed">Dashed Button</Button>
+        
+
+        <Divider type="vertical" />
+        <Button type="primary" icon={<SearchOutlined />}>
+          Search
+        </Button>
+        <Divider type="vertical" />
+        <Button href="#">Clear</Button>
+      
+
+    
       </Menu>
     </Header>
        
         <>
         <Layout>
             <Content>Content</Content>
-          </Layout>
-
-            <Layout>
-                <Layout>
-                <Content>Content</Content>
-            </Layout>
-                
-                <Content>Content</Content>
-
-              <Footer>Footer</Footer>
         </Layout>
 
+            <Layout>
+                <Content>Content</Content>
+            </Layout>
 
+                <Layout>
+                  <Content>Content</Content>
+                </Layout>
 
+                    <Layout>
+                      <Content>Content</Content>
+                    </Layout>
 
+                        <Layout>
+                            <Content>Content</Content>
+                        </Layout>
+
+                            <Layout>
+                              <Content>Content</Content>
+                            </Layout>
+
+                                <Layout>
+                                  <Content>Content</Content>
+                                </Layout>
+
+                                    <Layout>
+                                        <Content>Content</Content>
+                                    </Layout>
+
+                                        <Layout>
+                                          <Content>Content</Content>
+                                        </Layout>
+
+                                            <Layout>
+                                              <Content>Content</Content>
+                                            </Layout>
+
+                                              <Layout>
+                                                  <Content>Content</Content>
+                                              </Layout>
+                                              
+                                                  <Layout>
+                                                    <Content>Content</Content>
+                                                  </Layout>
+
+                                                       <Layout>
+                                                          <Content>Content</Content>
+                                                       </Layout>
+
+                                                  <Layout>
+                                                    <Content>Content</Content>
+                                                </Layout>
+
+                                            <Layout>
+                                              <Content>Content</Content>
+                                            </Layout>
+
+                                        <Layout>
+                                        <Content>Content</Content>
+                                     </Layout>
+
+                                <Layout>
+                                    <Content>Content</Content>
+                                </Layout>
+                                
+                                    <Layout>
+                                      <Content>Content</Content>
+                                    </Layout>
+
+                                       <Layout>
+                                          <Content>Content</Content>
+                                      </Layout>
+
+                                    <Layout>
+                                        <Content>Content</Content>
+                                    </Layout>
+
+                                <Layout>
+                                  <Content>Content</Content>
+                                </Layout>
+
+                          <Layout>
+                              <Content>Content</Content>
+                           </Layout>
+
+                      <Layout>
+                          <Content>Content</Content>
+                      </Layout>
+            
+                <Layout>
+                  <Content>Content</Content>
+                </Layout>
+
+              <Footer>Footer</Footer>
         </>
+
     </div>
   
   );
