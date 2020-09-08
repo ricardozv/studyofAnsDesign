0bc21799f46873ae82fd4792fcf658244e20a4ac
ricardozv <ricardosilvanet03@gmail.com>
2020-09-08 11:07:09 -0300

    realoca o esqueleto do spa

--- src/App.js
+++ src/App.js
@@ -1,12 +1,47 @@
 import React from 'react';
 import api from './services/api';
 
+import { Row, Col } from 'antd';
+import { Layout, Menu, Breadcrumb } from 'antd';
 import './global.css';
+
+
+const { SubMenu } = Menu;
+
+
+const { Header, Footer, Sider, Content } = Layout;
+
 
 function App() {
   return (
-    <div id="app">
+    <div className="App"> 
+    <Header className="header">
+      <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']}>
+        <Menu.Item key="1">Taxonomies</Menu.Item>
+        <Menu.Item key="2">API</Menu.Item>
+        <Menu.Item key="3">Documentation</Menu.Item>
+      </Menu>
+    </Header>
+       
+        <>
+        <Layout>
+            <Content>Content</Content>
+          </Layout>
 
+            <Layout>
+                <Layout>
+                <Content>Content</Content>
+            </Layout>
+                
+                <Content>Content</Content>
+
+              <Footer>Footer</Footer>
+        </Layout>
+
+
+
+
+        </>
     </div>
   
   );
