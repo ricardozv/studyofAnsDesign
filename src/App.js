8a24a63faa57d2dee76cae4649ed07d0da60e538
ricardozv <ricardosilvanet03@gmail.com>
2020-09-09 19:52:15 -0300

    muda o content da pagina, que eram cards e agora sao colunas.

--- src/App.js
+++ src/App.js
@@ -1,32 +1,55 @@
 // import api from './services/api';
+import { Menu } from 'antd';
 
-import { Divider, Space, AutoComplete } from 'antd';
+import { Row, Col } from 'antd';
+
+import { Divider, Space } from 'antd';
 import React from 'react';
 import { Button } from 'antd';
 import { SearchOutlined } from '@ant-design/icons';
-import { Layout, Menu} from 'antd';
-import './global.css';
-import { Card } from 'antd';
+import { Layout} from 'antd';
 
-// Verficar o reproveitamento das folhas de stylo dos cartões.
-const { Header, Footer, Content } = Layout;
+// import { PoweroffOutlined } from '@ant-design/icons';
+import './global.css';
+
+
+const { Header, Footer } = Layout;
+
+
+// tentando colocar reload nos butões 
+/* class App extends React.Component {
+  state = {
+    loadings: [],
+  };
+*/
+
+// Verficar o reproveitamento das folhas de stylo dos cartões
+
+
 
 function App() {
+ // const { loadings } = this.state; colocar reload nos butões 
   return (
     <div className="App"> 
     <Header className="header">
-      <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['1']}>
-      <Divider type="vertical" />
+      <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['100']}>
+      <Divider type="vertical"/>
         <Space>
-        <Button type= "primary">Taxonomies</Button>
-        <Button type="primary">Api</Button>
-        <Button type="primary">Documentasion</Button>
+       
+        <Button theme= "dark">Taxonomies</Button>
+        <Button theme="dark">Api</Button>
+        <Button theme="dark">Documentasion</Button>
         <Divider type="vertical" />
         </Space>
 
         <Space>
         <Divider type="vertical" />
-        <Button type="primary" icon={<SearchOutlined />}>
+        <Button 
+        type="primary" 
+        icon={<SearchOutlined />}
+        //loading={loadings[1]} tentativa de colocar reload no botão
+        onClick={() => this.enterLoading(1)}
+        >
           Search
         </Button>
         <Divider type="vertical" />
@@ -39,65 +62,187 @@ function App() {
     </Header>
        
         <>
+        <Row>
+          <Col span={6}>col-6</Col>
+          <Col span={6}>col-6</Col>
+          <Col span={6}>col-6</Col>
+          <Col span={6}>col-6</Col>
+        </Row>
 
-        
-        <Card style={
-          { 
-            width: 1200,
-            padding: 1
-          
-          }}>
-           <p>Card content</p>
-        </Card>
-        <Card style={
-          { 
-            width: 1200,
-            padding: 2
-          
-          }}>
-            <p>Card content</p>
-        </Card>
-        <Card style={
-          { 
-            width: 1200,
-            padding: 2
-          
-          }}>
-            <p>Card content</p>
-        </Card>
-        <Card style={
-          { 
-            width: 1200,
-            padding: 2
-          
-          }}>
-            <p>Card content</p>
-        </Card>
-        <Card style={
-          { 
-            width: 1200,
-            padding: 2
-          
-          }}>
-            <p>Card content</p>
-        </Card>
-        <Card style={
-          { 
-            width: 1200,
-            padding: 2
-          
-          }}>
-            <p>Card content</p>
-        </Card>
+        <Row>
+          <Col span={6}>col-6</Col>
+          <Col span={6}>col-6</Col>
+          <Col span={6}>col-6</Col>
+          <Col span={6}>col-6</Col>
+        </Row>
 
-        <Card style={
-          { 
-            width: 1200,
-            padding: 2
-          
-          }}>
-            <p>Card content</p>
-        </Card>
+        <Row>
+          <Col span={6}>col-6</Col>
+          <Col span={6}>col-6</Col>
+          <Col span={6}>col-6</Col>
+          <Col span={6}>col-6</Col>
+        </Row>
+
+        <Row>
+          <Col span={6}>col-6</Col>
+          <Col span={6}>col-6</Col>
+          <Col span={6}>col-6</Col>
+          <Col span={6}>col-6</Col>
+        </Row>
+
+        <Row>
+          <Col span={6}>col-6</Col>
+          <Col span={6}>col-6</Col>
+          <Col span={6}>col-6</Col>
+          <Col span={6}>col-6</Col>
+        </Row>
+
+
+        <Row>
+          <Col span={6}>col-6</Col>
+          <Col span={6}>col-6</Col>
+          <Col span={6}>col-6</Col>
+          <Col span={6}>col-6</Col>
+        </Row>
+
+
+        <Row>
+          <Col span={6}>col-6</Col>
+          <Col span={6}>col-6</Col>
+          <Col span={6}>col-6</Col>
+          <Col span={6}>col-6</Col>
+        </Row>
+
+        <Row>
+          <Col span={6}>col-6</Col>
+          <Col span={6}>col-6</Col>
+          <Col span={6}>col-6</Col>
+          <Col span={6}>col-6</Col>
+        </Row>
+
+        <Row>
+          <Col span={6}>col-6</Col>
+          <Col span={6}>col-6</Col>
+          <Col span={6}>col-6</Col>
+          <Col span={6}>col-6</Col>
+        </Row>
+
+        <Row>
+          <Col span={6}>col-6</Col>
+          <Col span={6}>col-6</Col>
+          <Col span={6}>col-6</Col>
+          <Col span={6}>col-6</Col>
+        </Row>
+
+        <Row>
+          <Col span={6}>col-6</Col>
+          <Col span={6}>col-6</Col>
+          <Col span={6}>col-6</Col>
+          <Col span={6}>col-6</Col>
+        </Row>
+
+        <Row>
+          <Col span={6}>col-6</Col>
+          <Col span={6}>col-6</Col>
+          <Col span={6}>col-6</Col>
+          <Col span={6}>col-6</Col>
+        </Row>
+
+
+        <Row>
+          <Col span={6}>col-6</Col>
+          <Col span={6}>col-6</Col>
+          <Col span={6}>col-6</Col>
+          <Col span={6}>col-6</Col>
+        </Row>
+
+
+        <Row>
+          <Col span={6}>col-6</Col>
+          <Col span={6}>col-6</Col>
+          <Col span={6}>col-6</Col>
+          <Col span={6}>col-6</Col>
+        </Row>
+
+        <Row>
+          <Col span={6}>col-6</Col>
+          <Col span={6}>col-6</Col>
+          <Col span={6}>col-6</Col>
+          <Col span={6}>col-6</Col>
+        </Row>
+
+        <Row>
+          <Col span={6}>col-6</Col>
+          <Col span={6}>col-6</Col>
+          <Col span={6}>col-6</Col>
+          <Col span={6}>col-6</Col>
+        </Row>
+
+        <Row>
+          <Col span={6}>col-6</Col>
+          <Col span={6}>col-6</Col>
+          <Col span={6}>col-6</Col>
+          <Col span={6}>col-6</Col>
+        </Row>
+
+        <Row>
+          <Col span={6}>col-6</Col>
+          <Col span={6}>col-6</Col>
+          <Col span={6}>col-6</Col>
+          <Col span={6}>col-6</Col>
+        </Row>
+
+        <Row>
+          <Col span={6}>col-6</Col>
+          <Col span={6}>col-6</Col>
+          <Col span={6}>col-6</Col>
+          <Col span={6}>col-6</Col>
+        </Row>
+
+
+        <Row>
+          <Col span={6}>col-6</Col>
+          <Col span={6}>col-6</Col>
+          <Col span={6}>col-6</Col>
+          <Col span={6}>col-6</Col>
+        </Row>
+
+
+        <Row>
+          <Col span={6}>col-6</Col>
+          <Col span={6}>col-6</Col>
+          <Col span={6}>col-6</Col>
+          <Col span={6}>col-6</Col>
+        </Row>
+
+        <Row>
+          <Col span={6}>col-6</Col>
+          <Col span={6}>col-6</Col>
+          <Col span={6}>col-6</Col>
+          <Col span={6}>col-6</Col>
+        </Row>
+
+        <Row>
+          <Col span={6}>col-6</Col>
+          <Col span={6}>col-6</Col>
+          <Col span={6}>col-6</Col>
+          <Col span={6}>col-6</Col>
+        </Row>
+
+        <Row>
+          <Col span={6}>col-6</Col>
+          <Col span={6}>col-6</Col>
+          <Col span={6}>col-6</Col>
+          <Col span={6}>col-6</Col>
+        </Row>
+
+        <Row>
+          <Col span={6}>col-6</Col>
+          <Col span={6}>col-6</Col>
+          <Col span={6}>col-6</Col>
+          <Col span={6}>col-6</Col>
+        </Row>
+
         <Layout>
               <Footer style={{
                 width: 1200,
--- src/components/rank/Rank.js
+++ src/components/rank/Rank.js
@@ -6,11 +6,11 @@ export default function Rank() {
     return(
         <div className="login-container">
             <form>
-            <img src={logo} alt = "Tindev" />
-            <input 
-            placeholder = "Digite seu nick do github"/>
 
-            <button type="submit"> enviar</button>
+            <input 
+            placeholder = "search"/>
+
+            <button type="submit">search</button>
             </form>
         </div>  
     );
