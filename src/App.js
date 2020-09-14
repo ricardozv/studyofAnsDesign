c122f1f2be67c415811a7000bdb8a19eab6b08f7
ricardozv <ricardosilvanet03@gmail.com>
2020-09-14 18:35:55 -0300

    introduçao da criaçao do component button mas nao funcionou, entao continuei trabalhando no supercomponente, vou desmenbra-lo, tenho lido sobre microserviços, quero aplicar essa arquitetura

--- src/App.js
+++ src/App.js
@@ -1,75 +1,71 @@
 // import api from './services/api';
 import { Menu } from 'antd';
-
+import './components/buttons/ButtonPrimary';
 import { Row, Col } from 'antd';
 
-import { Divider, Space } from 'antd';
+import { PageHeader, Button } from 'antd';
+
+// import que foram copiados pro componente 
+// import { Divider, Space } from 'antd';
 import React from 'react';
-import { Button } from 'antd';
-import { SearchOutlined } from '@ant-design/icons';
+// import { Button } from 'antd';
+// import { SearchOutlined } from '@ant-design/icons';
 import { Layout} from 'antd';
 
 
+import { Tag } from 'antd';
 
-import { useState } from 'react';
+
+//import { useState } from 'react';
 
 // import { PoweroffOutlined } from '@ant-design/icons';
 import './global.css';
 
 
-const { Header, Footer } = Layout;
+const { Footer } = Layout;
 
 
-// tentando colocar reload nos butões 
-/* class App extends React.Component {
-  state = {
-    loadings: [],
-  };
-*/
 
-// Verficar o reproveitamento das folhas de stylo dos cartões
+
 
 
 
 function App() {
- // const { loadings } = this.state; colocar reload nos butões 
- const [size] = useState(8);
+ // const [size] = useState(8);
   return (
-    <div className="App"> 
-    <Header className="header">
-      <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['100']}>
-      <Divider type="vertical"/>
-
-      
-        <Space size={size}>
-       
-        <Button theme= "dark">Taxonomies</Button>
-        <Button theme="dark">Api</Button>
-        <Button theme="dark">Documentasion</Button>
-        <Divider type="vertical" />
-        </Space>
-
-        <Space>
-        <Divider type="vertical" />
-        <Button 
-        type="primary" 
-        icon={<SearchOutlined />}
-        //loading={loadings[1]} tentativa de colocar reload no botão
-        onClick={() => this.enterLoading(1)}
-        >
-          Search
-        </Button>
-        <Divider type="vertical" />
-        <Button href="#">Clear</Button>
-        </Space>
-
+    <div style={{ width: 1200, margin: '40px auto' }}>
+       <div className="site-page-header-ghost-wrapper">
+    <PageHeader
+      ghost={true}
+      onBack={() => window.history.back()}
+      title="Back"
+      subTitle="This is a subtitle"
     
-      </Menu>
-      
-    </Header>
-       
+
+
+      extra={[
+        <Button theme= "dark">any rank</Button>,
+        <Button theme="dark">all divisions</Button>,
+        <Button key="3">Search</Button>,
+        <Button key="2">Search</Button>,
+        <Button key="1" type="primary">,
+          Clear
+        </Button>,
         <>
-        <Row gutter={[16, 16]}>
+      
+          <Tag color="#f50">#f50</Tag>
+        
+
+        </>
+      ]}
+    >
+    
+    </PageHeader>
+  </div>
+   
+    
+      <>
+        <Row gutter={[16, 16 ]}>
           <Col span={6}>col-6</Col>
           <Col span={6}>col-6</Col>
           <Col span={6}>col-6</Col>
@@ -198,10 +194,9 @@ function App() {
 
        
        
-        </>
-
-    </div>
-  
+    </>
+</div>
+   
   );
 }
 
--- src/components/buttons/ButtonPrimary.js
+++ src/components/buttons/ButtonPrimary.js
@@ -1,10 +1,47 @@
 import React from 'react';
-import './ButtonPrimary.js';
+import { Button } from 'antd';
+import { Divider, Space } from 'antd';
+import { SearchOutlined } from '@ant-design/icons';
+import './../../App.js';
+
+
+import { Menu } from 'antd';
+import { useState } from 'react';
+
+const { Header } = Menu;
 
 export default function ButtonPrimary() {
-
+    const [size] = useState(8);
     return (
+        <div style={{ width: 1200, margin: '40px auto' }}>
+        <div className="App"> 
+        <Header className="header">
+        <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['100']}>
+        <Space size={size}>
+       
+        <Button theme= "dark">Taxonomies</Button>
+        <Button theme="dark">Api</Button>
+        <Button theme="dark">Documentasion</Button>
+        <Divider type="vertical" />
+        </Space>
+
+        <Space>
+        <Divider type="vertical" />
+        <Button 
+        type="primary" 
+        icon={<SearchOutlined />}
+        >
+          Search
+        </Button>
+        <Divider type="vertical" />
+        <Button href="#">Clear</Button>
+        </Space>
+ 
+        </Menu>
         
-        
+      
+    </Header>
+    </div>
+    </div>
     );
 }
