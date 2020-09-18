7f2dd048c9099d3e2634f713d3bf1a2b8b81c993
ricardozv <ricardosilvanet03@gmail.com>
2020-09-18 00:33:12 -0300

    insere um botao header, deixa o content melhor estruturado

--- src/App.js
+++ src/App.js
@@ -1,31 +1,24 @@
 // import api from './services/api';
 import './components/buttons/ButtonPrimary';
-import { Row} from 'antd';
-
 import { PageHeader, Button } from 'antd';
-
 import React from 'react';
 import { Layout} from 'antd';
-
-
 import { Tag } from 'antd';
 import './global.css';
 
-
-
-
 const { Footer, Content } = Layout;
-
 
 function App() {
 
   return (
     <div style={{ width: 1200, margin: '40px auto' }}>
+        <Button theme= "dark">any rank</Button>
        <div className="site-page-header-ghost-wrapper">
     <PageHeader style={{ 
        background: '#0000FF',
        width: 1200,
        padding: 20
+       
 
     }}
       ghost={true}
@@ -33,6 +26,7 @@ function App() {
       title="Back"
       subTitle="This is a subtitle"
       extra={[
+        <Button style={{padding: 10, minHeight: 50 }}>any rank</Button>,
         <Button theme= "dark">any rank</Button>,
         <Button theme="dark">all divisions</Button>,
         <Button key="3">Search</Button>,
@@ -53,14 +47,21 @@ function App() {
     
       <>
 
-      <Content style={{ margin: '10px 50px 10px 6px', background:'#0000FF' }}>
+      <Content style={{ width: 1200, margin: '10px auto', background:'#0000FF' }}>
+            <div className="site-layout-background" style={{ padding: 20, minHeight: 50 }}>
+            <Button theme= "dark">any rank</Button>
+              
+            </div>
+          </Content>
+
+          <Content style={{ width: 1200, margin: '10px auto', background:'#0000FF' }}>
             <div className="site-layout-background" style={{ padding: 20, minHeight: 50 }}>
               Bill is a cat.
             kkkkkkkkkk
             </div>
           </Content>
 
-          <Content style={{ margin: '10px 50px 10px 6px', background:'#0000FF' }}>
+          <Content style={{ width: 1200, margin: '10px auto', background:'#0000FF' }}>
             <div className="site-layout-background" style={{ padding: 20, minHeight: 50 }}>
               Bill is a cat.
             kkkkkkkkkk
@@ -68,134 +69,65 @@ function App() {
           </Content>
 
 
-        <Row gutter={[16, 16 ]}>
-          <Col span={6}>col-6</Col>
-          <Col span={6}>col-6</Col>
-          <Col span={6}>col-6</Col>
-          <Col span={6}>col-6</Col>
-        </Row>
-
-        <Row>
-          <Col style={{ 
-            background:'#0000FF',
-            margin: '10px 50px 10px 6px',
-            padding: 20, 
-            minHeight: 50
-        
-        
-        }} span={6}>col-6</Col>
-         
-        </Row>
-
-        <Row gutter={[16, 16]}>
-          <Col span={6}>col-6</Col>
-          <Col span={6}>col-6</Col>
-          <Col span={6}>col-6</Col>
-          <Col span={6}>col-6</Col>
-        </Row>
-
-        <Row gutter={[16, 16]}>
-          <Col span={6}>col-6</Col>
-          <Col span={6}>col-6</Col>
-          <Col span={6}>col-6</Col>
-          <Col span={6}>col-6</Col>
-        </Row>
-
-        <Row gutter={[16, 16]}>
-          <Col span={6}>col-6</Col>
-          <Col span={6}>col-6</Col>
-          <Col span={6}>col-6</Col>
-          <Col span={6}>col-6</Col>
-        </Row>
+          <Content style={{ width: 1200, margin: '10px auto', background:'#0000FF' }}>
+            <div className="site-layout-background" style={{ padding: 20, minHeight: 50 }}>
+              Bill is a cat.
+            kkkkkkkkkk
+            </div>
+          </Content>
 
 
-        <Row gutter={[16, 16]}>
-          <Col span={6}>col-6</Col>
-          <Col span={6}>col-6</Col>
-          <Col span={6}>col-6</Col>
-          <Col span={6}>col-6</Col>
-        </Row>
+          <Content style={{ width: 1200, margin: '10px auto', background:'#0000FF' }}>
+            <div className="site-layout-background" style={{ padding: 20, minHeight: 50 }}>
+              Bill is a cat.
+            kkkkkkkkkk
+            </div>
+          </Content>
 
 
-        <Row gutter={[16, 16]}>
-          <Col span={6}>col-6</Col>
-          <Col span={6}>col-6</Col>
-          <Col span={6}>col-6</Col>
-          <Col span={6}>col-6</Col>
-        </Row>
+      <Content style={{ width: 1200, margin: '10px auto', background:'#0000FF' }}>
+            <div className="site-layout-background" style={{ padding: 20, minHeight: 50 }}>
+              Bill is a cat.
+            kkkkkkkkkk
+            </div>
+          </Content>
 
-        <Row gutter={[16, 16]}>
-          <Col span={6}>col-6</Col>
-          <Col span={6}>col-6</Col>
-          <Col span={6}>col-6</Col>
-          <Col span={6}>col-6</Col>
-        </Row>
+          <Content style={{ width: 1200, margin: '10px auto', background:'#0000FF' }}>
+            <div className="site-layout-background" style={{ padding: 20, minHeight: 50 }}>
+              Bill is a cat.
+            kkkkkkkkkk
+            </div>
+          </Content>
 
-        <Row gutter={[16, 16]}>
-          <Col span={6}>col-6</Col>
-          <Col span={6}>col-6</Col>
-          <Col span={6}>col-6</Col>
-          <Col span={6}>col-6</Col>
-        </Row>
-
-        <Row gutter={[16, 16]}>
-          <Col span={6}>col-6</Col>
-          <Col span={6}>col-6</Col>
-          <Col span={6}>col-6</Col>
-          <Col span={6}>col-6</Col>
-        </Row>
-
-        <Row gutter={[16, 16]}>
-          <Col span={6}>col-6</Col>
-          <Col span={6}>col-6</Col>
-          <Col span={6}>col-6</Col>
-          <Col span={6}>col-6</Col>
-        </Row>
-
-        <Row gutter={[16, 16]}>
-          <Col span={6}>col-6</Col>
-          <Col span={6}>col-6</Col>
-          <Col span={6}>col-6</Col>
-          <Col span={6}>col-6</Col>
-        </Row>
+          <Content style={{ width: 1200, margin: '10px auto', background:'#0000FF' }}>
+            <div className="site-layout-background" style={{ padding: 20, minHeight: 50 }}>
+              Bill is a cat.
+            kkkkkkkkkk
+            </div>
+          </Content>
 
 
-        <Row gutter={[16, 16]}>
-          <Col span={6}>col-6</Col>
-          <Col span={6}>col-6</Col>
-          <Col span={6}>col-6</Col>
-          <Col span={6}>col-6</Col>
-        </Row>
+          <Content style={{ width: 1200, margin: '10px auto', background:'#0000FF' }}>
+            <div className="site-layout-background" style={{ padding: 20, minHeight: 50 }}>
+              Bill is a cat.
+            kkkkkkkkkk
+            </div>
+          </Content>
 
 
-        <Row gutter={[16, 16]}>
-          <Col span={6}>col-6</Col>
-          <Col span={6}>col-6</Col>
-          <Col span={6}>col-6</Col>
-          <Col span={6}>col-6</Col>
-        </Row>
+          <Content style={{ mwidth: 1200, margin: '10px auto', background:'#0000FF' }}>
+            <div className="site-layout-background" style={{ padding: 20, minHeight: 50 }}>
+              Bill is a cat.
+            kkkkkkkkkk
+            </div>
+          </Content>
 
-        <Row gutter={[16, 16]}>
-          <Col span={6}>col-6</Col>
-          <Col span={6}>col-6</Col>
-          <Col span={6}>col-6</Col>
-          <Col span={6}>col-6</Col>
-        </Row>
-
-        <Row gutter={[16, 16]}>
-          <Col span={6}>col-6</Col>
-          <Col span={6}>col-6</Col>
-          <Col span={6}>col-6</Col>
-          <Col span={6}>col-6</Col>
-        </Row>
-
-     
 
         <Layout>
               <Footer style={{
                 background: '#0000FF',
                 width: 1200,
-                padding: 8
+                padding: 20
               }}>
                 Footer</Footer>
         </Layout>
