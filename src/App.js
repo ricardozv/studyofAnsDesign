9a8e8aa28d6243abda30b78831f75636700e27f2
ricardozv <ricardosilvanet03@gmail.com>
2020-09-22 02:04:32 -0300

    cria os seletectes da pagina

--- src/App.js
+++ src/App.js
@@ -4,40 +4,15 @@ import { PageHeader, Button } from 'antd';
 import React from 'react';
 import { Layout} from 'antd';
 import './global.css';
+import { Select } from 'antd';
 
-
-import { Menu, Dropdown } from 'antd';
-import { DownOutlined } from '@ant-design/icons';
-
-
-
-const menu = (
-  <Menu onClick={handleMenuClick}>
-    <Menu.Item key="1">1st item</Menu.Item>
-    <Menu.Item key="2">2nd item</Menu.Item>
-    <Menu.Item key="3">3rd item</Menu.Item>
-  </Menu>
-);
-
-function handleMenuClick(e) {
-  return (
-    <>
-    <Button type="primary">primary</Button>
-    <Button>secondary</Button>
-    <Dropdown overlay={menu}>
-      <Button>
-        Actions <DownOutlined />
-      </Button>
-    </Dropdown>
-  </>
-  )
-}
-
-
-
+const { Option } = Select;
 const { Footer, Content } = Layout;
 
 function App() {
+  function handleChange(value) {
+    console.log(`selected ${value}`);
+  }
 
   return (
     <div style={{ width: 1200, margin: '40px auto' }}>
@@ -51,26 +26,34 @@ function App() {
     }}
     
       extra={[
+        <>
+          
+            <Button type="link">Taxonomies </Button>,
+                <Button type="link">Documentation</Button>,
+            <Button type="link">Api</Button>,
 
-        <Button type="link">Taxonomies </Button>,
-        <Button type="link">Documentation</Button>,
-        <Button type="link">Api</Button>,
+            <Select defaultValue="any rank" style={{ width: 120 }} onChange={handleChange}>
+                <Option value="jack">any rank</Option>
+                  <Option value="lucy">Lucy</Option>
+              <Option value="Yiminghe">yiminghe</Option>
+            </Select>
 
-
-        <Button theme= "dark">any rank</Button>,
-        <Button theme="dark">all divisions</Button>,
-        <Button key="3">Search</Button>,
-        <Button key="2">Search</Button>,
-        <Button key="1" type="primary">
-          Clear
-        </Button>,
+            <Select defaultValue="all divisions" style={{ width: 120 }} onChange={handleChange}>
+                <Option value="jack">any rank</Option>
+                  <Option value="lucy">Lucy</Option>
+              <Option value="Yiminghe">yiminghe</Option>
+            </Select>  
+        
+               <Button key="2">Search</Button>,
+                   <Button key="1" type="primary">
+                      Clear
+            </Button>,
+        </>
 
       ]}>
     
     </PageHeader>
   </div>
-   
-    
       <>
           <Content style={{ width: 1200, margin: '10px auto', background:'#0000FF' }}>
             <div className="site-layout-background" style={{ padding: 20, minHeight: 50 }}>
@@ -103,7 +86,7 @@ function App() {
           </Content>
 
 
-      <Content style={{ width: 1200, margin: '10px auto', background:'#0000FF' }}>
+           <Content style={{ width: 1200, margin: '10px auto', background:'#0000FF' }}>
             <div className="site-layout-background" style={{ padding: 20, minHeight: 50 }}>
               Bill is a cat.
             kkkkkkkkkk
@@ -149,12 +132,8 @@ function App() {
               }}>
                 Footer</Footer>
         </Layout>
-        
-
-        
        
-       
-    </>
+    </>,
 </div>
    
   );
